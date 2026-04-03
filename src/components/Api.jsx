6a16f64b33a90b9ceac9f2fc960f92a4f583e49a
ikosong14 <alexandervirtuous14@gmail.com>
2026-04-03 02:02:39 +0100

    modified:   src/components/Api.jsx

--- src/components/Api.jsx
+++ src/components/Api.jsx
@@ -1,4 +1,4 @@
 // const Api = "http://192.168.64.16:5000/client";
-const Api = "http://test.trashitintegrated.com/client";
+const Api = "https://generalbackend-9byl.onrender.com/client";
 
 export default Api;
